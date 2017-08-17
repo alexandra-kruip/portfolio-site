b93f43823a434857ccb03b42fa598ad1d1f660c8
Alexandra Kruip <alexkruip@gmail.com>
2017-08-17 13:19:46 -0700

    minor changes, removed contact form

--- js/contact.js
+++ js/contact.js
@@ -11,11 +11,13 @@ function email(){
     e += 'gmail.com';
     $('.email').text(e);
     e = 'mailto:' + e;
-    $('.email ').attr('href', e);
+    $('.email').attr('href', e);
 }
 function phone(){
     var p = '714';
     p += '-592';
     p += '-4742';
     $('.phone').text(p);
+    n = 'tel:+1' + '714' + '592' + '4742';
+    $('.phone').attr('href', n)
 }
